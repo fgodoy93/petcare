65a30b53ea9645aa435e22d98cdf0ca16db7faf7
Francisco Godoy <fgodoyr93@gmail.com>
2025-12-08 01:03:07 -0300

    se añade .sequelizer para despliegue en render

---
+++ src/config/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    logging: false
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
